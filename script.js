ec8a903a7f0fd10fbbcc7eec6bee000aa73ab363
alt-potato <jason.why.zhuang@gmail.com>
2025-10-07 13:48:31 -0400

    Add color messages

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 function greetUser() {
-  let name = document.getElementById("nameInput").value;
-  if (name.trim() === "") {
+  let name = document.getElementById("nameInput").value.trim();
+  let color = document.getElementById("colorInput").value.trim();
+
+  if (name === "") {
     alert("Please enter your name first!");
-  } else {
-    alert("Hello, " + name + "!");
+  } 
+
+  switch (color.toLowerCase()) {
+    case "red":
+      alert("Hello, " + name + "! Red is a great color.");
+      break;
+    case "blue":
+      alert("Hello, " + name + "! Blue is nice, but it's just... so blasé.");
+      break;
+    case "green":
+      alert("Hello, " + name + "! Green is pretty cringe ngl.");
+      break;
+    case "fuchsia":
+      alert("Hello, " + name + "! That is not your favorite color.");
+      break;
+    default:
+      alert("Hello, " + name + "! Your favorite color is " + color + ".");
+      break;
   }
 }
 
