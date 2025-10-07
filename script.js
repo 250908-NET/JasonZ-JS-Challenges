445f0ccdf9730a2539a4588ebec6b138adb23939
alt-potato <jason.why.zhuang@gmail.com>
2025-10-07 13:50:15 -0400

    Add calculator example

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-function greetUser() {
-  let name = document.getElementById("nameInput").value.trim();
-  let color = document.getElementById("colorInput").value.trim();
+function addNumbers() {
+  let n1 = Number(document.getElementById("num1").value);
+  let n2 = Number(document.getElementById("num2").value);
+  let sum = n1 + n2;
 
-  if (name === "") {
-    alert("Please enter your name first!");
-  } 
-
-  switch (color.toLowerCase()) {
-    case "red":
-      alert("Hello, " + name + "! Red is a great color.");
-      break;
-    case "blue":
-      alert("Hello, " + name + "! Blue is nice, but it's just... so blasé.");
-      break;
-    case "green":
-      alert("Hello, " + name + "! Green is pretty cringe ngl.");
-      break;
-    case "fuchsia":
-      alert("Hello, " + name + "! That is not your favorite color.");
-      break;
-    default:
-      alert("Hello, " + name + "! Your favorite color is " + color + ".");
-      break;
-  }
+  document.getElementById("result").textContent = "Result: " + sum;
 }
 
-document.getElementById("greetBtn").addEventListener("click", greetUser);
+document.getElementById("addBtn").addEventListener("click", addNumbers);
