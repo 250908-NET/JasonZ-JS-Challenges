863c66b5029814a87ab1638aa9b409f3e1cf5c86
alt-potato <jason.why.zhuang@gmail.com>
2025-10-07 16:00:14 -0400

    Add random post button

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-async function _loadPosts() {
-  let posts = await fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .catch((error) => console.error("Error fetching data:", error));
-
-  posts.slice(0, 5).forEach((post) => {
-    const postDiv = document.createElement("div");
-    postDiv.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
-    document.getElementById("output").appendChild(postDiv);
-  });
-}
+const numPosts = fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res) => res.json())
+  .then((data) => data.length);
 
 async function loadPost(id) {
   let post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -39,4 +31,10 @@ document.getElementById("loadBtn").addEventListener("click", async () => {
       return createError(`<p>Post ${id} not found.</p>`);
     })
   );
+});
+
+document.getElementById("randomBtn").addEventListener("click", async () => {
+  const id = Math.floor(Math.random() * (await numPosts)) + 1;
+  document.getElementById("idInput").value = id;
+  document.getElementById("loadBtn").click();
 });
