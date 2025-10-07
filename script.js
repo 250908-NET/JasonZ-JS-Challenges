eca6d547b57056b0bf1e36dc0c59cf9c61cea2e1
alt-potato <jason.why.zhuang@gmail.com>
2025-10-07 15:03:16 -0400

    Add fetch template code

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-let input = document.getElementById("taskInput");
-let addButton = document.getElementById("addTask");
-let clearButton = document.getElementById("clearCompleted");
-let list = document.getElementById("taskList");
-
-addButton.addEventListener("click", function () {
-  let taskText = input.value.trim();
-  if (taskText === "") return;
-
-  let li = document.createElement("li");
-  li.textContent = taskText;
-
-  li.addEventListener("click", function () {
-    li.classList.toggle("done");
-  });
-
-  list.appendChild(li);
-  input.value = "";
-});
-
-clearButton.addEventListener("click", function () {
-  let doneItems = document.getElementsByClassName("done");
-  while (doneItems.length > 0) {
-    list.removeChild(doneItems[0]);
-  }
-});
-
-const _makeBook = (title, author) => ({ title: title, author: author });
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Fetched post:', data);
+  })
+  .catch(error => console.error('Error fetching data:', error));
