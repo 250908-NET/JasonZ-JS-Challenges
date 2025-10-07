5cf62287e54f6fe528495f96f7830f97b71fcd24
alt-potato <jason.why.zhuang@gmail.com>
2025-10-07 15:12:05 -0400

    Add fetch output to page content

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(response => response.json())
-  .then(data => {
-    console.log('Fetched post:', data);
-  })
+  .then(data => document.getElementById('output').innerHTML = `<h2>${data.title}</h2><p>${data.body}</p>`)
   .catch(error => console.error('Error fetching data:', error));
