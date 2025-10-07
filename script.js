ee905c66d38eac226b640cdb43711a6be8781587
alt-potato <jason.why.zhuang@gmail.com>
2025-10-07 15:00:09 -0400

    Create makeBook function

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let addButton = document.getElementById("addTask");
 let clearButton = document.getElementById("clearCompleted");
 let list = document.getElementById("taskList");
 
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function () {
   let taskText = input.value.trim();
   if (taskText === "") return;
 
   let li = document.createElement("li");
   li.textContent = taskText;
 
-  li.addEventListener("click", function() {
+  li.addEventListener("click", function () {
     li.classList.toggle("done");
   });
 
@@ -18,13 +18,11 @@ addButton.addEventListener("click", function() {
   input.value = "";
 });
 
-clearButton.addEventListener("click", function() {
+clearButton.addEventListener("click", function () {
   let doneItems = document.getElementsByClassName("done");
   while (doneItems.length > 0) {
     list.removeChild(doneItems[0]);
   }
 });
 
-const nums = [10, 15, 20, 25, 30];
-
-console.log(nums.filter(x => x % 2 == 0));
+const _makeBook = (title, author) => ({ title: title, author: author });
