b0e5a8c62ad8327099a9ecfc5581db95d5d0bb52
alt-potato <jason.why.zhuang@gmail.com>
2025-10-09 16:00:25 -0400

    Add more button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const numPosts = fetch("https://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
   .then((data) => data.length);
 
+let last = 1;
+
 async function loadPost(id) {
   let post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then((res) => {
@@ -21,17 +23,29 @@ function createError(innerHTML) {
   return errorDiv;
 }
 
-document.getElementById("loadBtn").addEventListener("click", async () => {
-  const id = document.getElementById("idInput").value || 1;
+async function insertPost(fallback = 1, reset = true) {
+  const id =
+    parseInt(document.getElementById("idInput").value.trim()) || fallback;
   document.getElementById("idInput").value = "";
 
-  document.getElementById("output").innerHTML = "";
+  if (reset) document.getElementById("output").innerHTML = "";
+
   document.getElementById("output").appendChild(
     await loadPost(id).catch(() => {
       return createError(`<p>Post ${id} not found.</p>`);
     })
   );
-});
+
+  last = id >= 1 ? id : 1;
+}
+
+document
+  .getElementById("loadBtn")
+  .addEventListener("click", async () => insertPost());
+
+document
+  .getElementById("moreBtn")
+  .addEventListener("click", async () => insertPost(last + 1, false));
 
 document.getElementById("randomBtn").addEventListener("click", async () => {
   const id = Math.floor(Math.random() * (await numPosts)) + 1;
